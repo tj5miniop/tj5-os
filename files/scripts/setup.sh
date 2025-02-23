6979f83c820ff1d4463d24bd03b8c4729558c1b5
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-02-23 20:36:29 +0000

    Add steam to NVIDIA & AMD images
    native steam runs better than flatpak, so I may as well use that

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -7,3 +7,15 @@ set -oue pipefail
 
 # Your code goes here.
 wget -qO- https://git.io/papirus-icon-theme-install | sh
+
+#Install Steam
+# Perform a distro-sync (update) (again)
+dnf5 -y update
+dnf5 -y upgrade
+#Install Steam - taken from fedora copr repo - cannot be used in tj5-os due to way it's built currently
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 -y update --refresh
+
+ #Install Steam Package
+dnf5 -y install steam --allowerasing
